dbd5ec23b8f9d2b67cdab54b10c5ab28d92bbe58
root <root@localhost.localdomain>
2026-07-04 21:52:08 +0000

    fix: IServerBrowser 4 override methods + arm64 r19c sysroot
    - IServerBrowser.h: add GetMapFriendlyNameAndGameType, SetWorkshopEnabled,
      AddWorkshopSubscribedMap, RemoveWorkshopSubscribedMap (needed by ServerBrowser.h OVERRIDE)
    - arm64: r19c sysroot is toolchains/llvm/prebuilt/linux-x86_64/sysroot, not platforms/
    - arm64: remove --sysroot from CC (clang uses built-in sysroot)

--- engine/serverbrowser/ServerBrowser/IServerBrowser.h
+++ engine/serverbrowser/ServerBrowser/IServerBrowser.h
@@ -1,4 +1,4 @@
-// IServerBrowser.h - Server browser interface stub
+// IServerBrowser.h - Server browser interface
 #ifndef ISERVERBROWSER_H
 #define ISERVERBROWSER_H
 
@@ -6,6 +6,10 @@ class IServerBrowser
 {
 public:
 	virtual ~IServerBrowser() {}
+	virtual const char *GetMapFriendlyNameAndGameType(const char *pszMapName, char *szFriendlyMapName, int cchFriendlyName) = 0;
+	virtual void SetWorkshopEnabled(bool bManaged) = 0;
+	virtual void AddWorkshopSubscribedMap(const char *pszMapName) = 0;
+	virtual void RemoveWorkshopSubscribedMap(const char *pszMapName) = 0;
 };
 
 #define SERVERBROWSER_INTERFACE_VERSION "ServerBrowser003"
